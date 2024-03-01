a8ad6430d5422ae7b7b22dc5ff2c6fc1d278ffc3
ManFiZz <vovka79025315990@gmail.com>
2024-03-01 17:44:43 +0800

    init empty app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AppInitializers from './js/AppInitializers';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AppInitializers />
+  </React.StrictMode>
+);
---
+++ src/js/AppInitializers.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import '../css/main.css';
+
+class AppInitializers extends Component {
+  render() {
+    return (
+    <>
+    </>
+  )};
+}
+
+export default AppInitializers;
